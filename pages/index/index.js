869dec93ce084558606207930d524841a86233db
Andrew Y Xie <andyxie@D-69-91-175-59.dhcp4.washington.edu>
2017-05-30 14:38:07 -0700

    header

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,16 @@
+var app = getApp();
+
 Page({
   data: {
-    imgUrls: [
-      'http://image.huanqiuxiaozhen.com/%E7%84%A6%E7%82%B9%E5%9B%BE/2017_5_19_1495174115476',
-      'http://image.huanqiuxiaozhen.com/%E7%84%A6%E7%82%B9%E5%9B%BE/2017_5_19_1495174386313',
-      'http://image.huanqiuxiaozhen.com/%E7%84%A6%E7%82%B9%E5%9B%BE/2017_2_6_1486352315488', 
-      'http://image.huanqiuxiaozhen.com/%E7%84%A6%E7%82%B9%E5%9B%BE/2016_9_18_1474194376998'
-    ],
-    recommendedUrls:[
-      'http://image.huanqiuxiaozhen.com/%E4%B8%BB%E9%A2%98%E9%A6%86%E5%9B%BE/2017_5_16_1494917892270',
-      'http://image.huanqiuxiaozhen.com/%E4%B8%BB%E9%A2%98%E9%A6%86%E5%9B%BE/2017_5_16_1494917926060',
-      'http://image.huanqiuxiaozhen.com/%E4%B8%BB%E9%A2%98%E9%A6%86%E5%9B%BE/2017_5_16_1494926657165',
-      'http://image.huanqiuxiaozhen.com/%E4%B8%BB%E9%A2%98%E9%A6%86%E5%9B%BE/2017_5_16_1494926955020',
-      'http://image.huanqiuxiaozhen.com/%E4%B8%BB%E9%A2%98%E9%A6%86%E5%9B%BE/2017_5_16_1494918074908',
-      'http://image.huanqiuxiaozhen.com/%E4%B8%BB%E9%A2%98%E9%A6%86%E5%9B%BE/2017_5_16_1494918103781'
-    ]
+    userInfo: {}
   },
+  onLoad: function() {
+    var that = this;
+    app.getUserInfo(function(data){
+      that.setData({
+        userInfo: data
+      });
+      console.log(data);
+    });
+  }
 })
