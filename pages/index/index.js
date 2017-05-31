144a1551799fdf85da17a3b555c3ee83dee8532d
Andrew Y Xie <andyxie@D-69-91-175-59.dhcp4.washington.edu>
2017-05-30 19:22:29 -0700

    timer

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,15 @@ var app = getApp();
 Page({
   data: {
     userInfo: {},
-    current: 1,
+    x: 0,
+    y: 0,
+    z: 0,
+    time: 0,
+    timeStr: "00:00:00",
+    timer: null,
+    current: null,
+    isScreenFacingDown: false,
+    isTimerStarted: false,
     category: [
       {
         id: "学",
@@ -42,7 +50,8 @@ Page({
       },
       {
         id: "计",
-        color: "计划一天或者处理邮件。虽然很讨厌，但是积压的事情必须完成。"
+        description: "计划一天或者处理邮件。虽然很讨厌，但是积压的事情必须完成。",
+        color: "purple"
       },
       {
         id: "睡",
@@ -57,7 +66,87 @@ Page({
       that.setData({
         userInfo: data
       });
-      console.log(data);
+    });
+  },
+  handleTap: function(e){
+    var that = this;
+    this.setData({
+      current: e.target.dataset.index,
+    });
+    wx.onAccelerometerChange(this.processAccelerometerChange);
+  },
+  processAccelerometerChange: function(res){
+    this.setData({
+      x: res.x,
+      y: res.y,
+      z: res.z
+    });
+    // if(true){
+    if (res.z > 0.9) {  
+      this.startTimer();
+      this.setData({
+        isScreenFacingDown: true,     
+      });
+    }else{
+      this.setData({
+        isScreenFacingDown: false,
+      });
+      this.pauseTimer();
+    }
+  },
+  startTimer: function() {
+    var that = this;
+    if (!this.data.isTimerStarted){
+      this.setData({
+        isTimerStarted: true
+      })
+      this.timer = setInterval(function () {
+        that.setData({
+          time: that.data.time + 1,
+        })
+        that.updateTimeStr();
+      }, 1000);
+    }  
+  },
+  pauseTimer: function() {
+    if (this.data.isTimerStarted) {
+      clearInterval(this.timer);
+      this.setData({
+        isTimerStarted: false
+      });
+    }     
+  },
+  updateTimeStr: function() {
+    var t = this.data.time;
+    var hour, minute, second = 0;
+    var addDigit = function(n){
+      if (n < 10) {
+        return "0" + n;
+      }else{
+        return "" + n;
+      }
+    }
+    var hour = addDigit(Math.floor(t /3600));
+    var minute = addDigit(Math.floor(t%3600/60));
+    var second = addDigit(t - 3600*hour - 60*minute);
+    this.setData({
+      timeStr: "" + hour + ":" + minute + ":" + second
+    });
+  },
+  stopTimer: function() {
+    console.log("flag");
+    wx.stopAccelerometer();
+    this.pauseTimer();
+
+    this.setData({
+      isTimerStarted: false,
+      isScreenFacingDown: false,
+      time: 0,
+      timeStr: "00:00:00",
+      timer: null,
+      current: null,
     });
   }
+  
+
 })
